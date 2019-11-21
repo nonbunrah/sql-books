4e9458236411f4c007631d63141076ee8fb6dbb2
brockwc <brock.whitbread@gmail.com>
2019-11-21 00:47:52 -0800

    refactors two routes

--- index.js
+++ index.js
@@ -11,34 +11,6 @@ app.use(express.json());
 const port = 3000;
 
 
-// Temp Data
-const books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-let newBookUUID = 18;
-
-
 // Routes
 app.get('/', (request, response) => {
   response.send('Visit /api/books to see our list of titles');
@@ -47,60 +19,56 @@ app.get('/', (request, response) => {
 // get all books
 app.get('/api/books',  (req, res) => {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  const getAllBooks = "SELECT * FROM books";
+
+  database.all(getAllBooks, (error, results) => {
+    if (error) {
+      console.log(new Error('Could not get all books'), error);
+      res.sendStatus(500);
+    }
+    res.status(200).json(results);
+  });
 });
 
 // get one book
 app.get('/api/books/:id',  (req, res) => {
   // find one book by its id
-  console.log('books show', req.params);
-  for(let i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+
 });
 
 // create new book
 app.post('/api/books',  (req, res) => {
-  // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  const newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
+  // create new book with data (`req.body`)
+
 });
 
 // update book
 app.put('/api/books/:id', (req,res) => {
-// get book id from url params (`req.params`)
-  console.log('books update', req.params);
-  const bookId = req.params.id;
-  // find the index of the book we want to remove
-  const updateBookIndex = books.findIndex((element, index) => {
-    return (element._id === parseInt(req.params.id)); //params are strings
+  // get book id from url params (`req.params`)
+
+  // const queryHelper = Object.keys.map(ele => {
+  //   return `${ ele.toUpperCase() } = ?`;
+  // });
+  // const updateOneBook = `UPDATE books SET ${queryHelper.join(' ')} WHERE books.oid = ${req.params.id}`;
+  // database.run(updateOneBook, Object.values(req.body), error => {
+
+  const updateOneBook = `UPDATE books SET TITLE = ? WHERE books.oid = ${req.params.id}`;
+
+  database.run(updateOneBook, [req.body.title], error => {
+    if (error) {
+      console.log(new Error('Could not update book'), error);
+      res.sendStatus(500);
+    } else {
+      console.log(`Book with ID ${req.params.id} was update successfully`);
+      res.sendStatus(200);
+    }
   });
-  console.log('updating book with index', deleteBookIndex);
-  const bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
 });
 
 // delete book
 app.delete('/api/books/:id',  (req, res) => {
   // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  const bookId = req.params.id;
-  // find the index of the book we want to remove
-  const deleteBookIndex = books.findIndex((element, index) => {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  const bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
+
 });
 
 
